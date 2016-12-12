3f605649cfa400f4100892ee0f4bd626c6315a7e
Matthew <marbaugh@gmail.com>
2016-12-11 20:40:44 -0500

    Removed 12/12 session and updated the A-Team email to be 6pm

--- send_mail.php
+++ send_mail.php
@@ -36,7 +36,7 @@ s<?php
     $req_msg .= "Name: ".($customer_name)."\n";
     $req_msg .= "Email: ".($customer_email)."\n";
     $req_msg .= "Phone: ".($customer_phone)."\n";
-    $req_msg .= "When: ".($requested_date)." 5:30pm \n";
+    $req_msg .= "When: ".($requested_date)." 6:00pm \n";
     // $req_msg .= "Comments: ".($comments)."\n";
 
     // Set up the confirmation subject and mail headers
